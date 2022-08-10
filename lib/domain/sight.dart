b1f025cbc0b990838ec4f75af1e38347abb9f2c4
Ilya Vasilyev <den11krs@gmail.com>
2022-08-11 04:16:47 +0700

    Implement Sight class and a mock list with instances of this class

---
+++ lib/domain/sight.dart
@@ -0,0 +1,17 @@
+class Sight {
+  final String name;
+  final double lat;
+  final double lon;
+  final String url;
+  final String details;
+  final String type;
+
+  Sight(
+    this.name,
+    this.lat,
+    this.lon,
+    this.url,
+    this.details,
+    this.type,
+  );
+}
---
+++ lib/mocks.dart
@@ -0,0 +1,28 @@
+import 'package:places/domain/sight.dart';
+
+final List<Sight> mocks = [
+  Sight(
+    'Acueducto de Segovia',
+    40.94800736932299,
+    -4.117791526511193,
+    'https://upload.wikimedia.org/wikipedia/commons/c/c9/Acueducto_Romano_%28Segovia%2C_España%29.jpg',
+    'El acueducto de Segovia es un acueducto romano que llevaba aguas a la ciudad española de Segovia hasta 1973. Su construcción data de principios del siglo ii d. C., a finales del reinado del emperador Trajano o principios del de Adriano. La parte más visible, y por lo tanto famosa, es la arquería que cruza la plaza del Azoguejo, en la ciudad.',
+    'Roman aqueduct & grand landmark monument',
+  ),
+  Sight(
+    'Alhambra',
+    37.17624068225215,
+    -3.588141295597747,
+    'https://upload.wikimedia.org/wikipedia/commons/9/9f/Panor%C3%A1mica_Alhambra_y_Sierra_Nevada_de_fondo.jpg',
+    'a palace and fortress complex located in Granada, Andalusia, Spain. It is one of the most famous monuments of Islamic architecture and one of the best-preserved palaces of the historic Islamic world.',
+    'Medieval complex housing palaces, carved Moorish throne room & cloistered courtyards with fountains',
+  ),
+  Sight(
+    'La Ciudad de las Artes y las Ciencias',
+    39.45493273656893,
+    -0.3504675224226168,
+    'https://upload.wikimedia.org/wikipedia/commons/e/ef/La_Ciudad_de_las_Artes_y_las_Ciencias_de_Valencia.jpg',
+    'State-of-the-art science complex set within a landscaped park with a huge aquarium & IMAX screen.',
+    'un complejo arquitectónico, cultural y de entretenimiento',
+  ),
+];
